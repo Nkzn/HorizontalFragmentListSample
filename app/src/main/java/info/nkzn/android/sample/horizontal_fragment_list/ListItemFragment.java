02a8a2cd0e7c1c319436f3d7f68601a9a14aae8d
Yukiya Nakagawa <y.nakagawa@water-cell.jp>
2015-05-27 19:48:25 +0900

    Add sample fragment

---
+++ app/src/main/java/info/nkzn/android/sample/horizontal_fragment_list/ListItemFragment.java
@@ -0,0 +1,16 @@
+package info.nkzn.android.sample.horizontal_fragment_list;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ListItemFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_list_item, container, false);
+    }
+}
